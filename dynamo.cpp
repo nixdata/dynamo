c9e94fd5f1d9eb00644c9caec719164d6f947d7d
Phil Peron <peron@protonmail.ch>
2017-07-23 21:59:04 -0400

    Added time step to dmo_run(). It's alive. Sort of.

--- dynamo.cpp
+++ dynamo.cpp
@@ -6,7 +6,10 @@
 
 
 static bool running;
-static struct dmo_time dmo_time;
+static struct dmo_time current_time;
+static struct dmo_time total_time;
+static struct dmo_time delta_time;
+static struct dmo_time accum_time;
 
 
 // TODO: Move this into dmo_sys
@@ -20,9 +23,8 @@ void interrupt_handler(int signal)
 int dmo_startup() 
 {
     running = false;
-    dmo_time = {0};
 
-    printf("Starting dynamo...\n");
+    printf("Starting Dynamo...\n");
 
     signal(SIGINT, interrupt_handler);
     dmo_sys_init();
@@ -36,9 +38,49 @@ int dmo_startup()
 void dmo_run() 
 {
     running = true;
+    current_time = {0};
+    total_time = {0};
+    delta_time = {0};
+    accum_time = {0};
+
+    // struct dmo_state previous_state;
+    // struct dmo_state current_state;
 
     while(running) {
+        dmo_time new_time = dmo_sys_time();
+        dmo_time frame_time = {0};
 
+        frame_time.seconds = new_time.seconds - current_time.seconds;
+        frame_time.nanoseconds = new_time.nanoseconds - current_time.nanoseconds;
+        // TODO: Figure out frame_time clamp below.
+        //if(frame_time > some_clamped_value) { frame_time = some_clamped_value; }
+
+        current_time.seconds = new_time.seconds;
+        current_time.nanoseconds = new_time.nanoseconds;
+
+        accum_time.seconds += frame_time.seconds;
+        accum_time.nanoseconds += frame_time.nanoseconds;
+
+        while((accum_time.seconds >= delta_time.seconds) && (accum_time.nanoseconds >= delta_time.nanoseconds)) {
+
+            //previous_state = current_state;
+
+            // simulate current_state with delta_time;
+            // dmo_sim_step(current_state, total_time, delta_time);
+
+            total_time.seconds += delta_time.seconds;
+            total_time.nanoseconds += delta_time.nanoseconds;
+            accum_time.seconds -= delta_time.seconds;
+            accum_time.nanoseconds -= delta_time.nanoseconds;
+        }
+
+        const double alpha_seconds = accum_time.seconds / delta_time.seconds;
+        const double alpha_nanoseconds = accum_time.nanoseconds / delta_time.nanoseconds;
+        (void)alpha_seconds; 
+        (void)alpha_nanoseconds;
+        
+        // dmo_state alpha_state = current_state * alpha + previous_state * (1.0 - alpha_state);
+        // dmo_gfx_render(current_state);
     }
 }
 
@@ -47,7 +89,7 @@ int dmo_shutdown()
 {
     running = false;
 
-    printf("Shutting down dynamo...\n");
+    printf("\nShutting down Dynamo...\n");
     printf("Dynamo off!\n");
 
     exit(0);
