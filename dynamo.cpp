c9c44b0d48a4028d8d68efcc2ad39a75329e8fd7
Phil Peron <peron@protonmail.ch>
2017-07-24 21:25:47 -0400

    Simplified timers.

--- dynamo.cpp
+++ dynamo.cpp
@@ -10,10 +10,6 @@
 
 
 static bool running = false;
-static struct dmo_time current_time;
-static struct dmo_time total_time;
-static struct dmo_time fixed_time;
-static struct dmo_time accum_time;
 
 
 // TODO: Move this into dmo_sys
@@ -42,50 +38,46 @@ int dmo_startup()
 void dmo_run() 
 {
     running = true;
-    dmo_time base_time = dmo_sys_time();
-    current_time = {0};
-    total_time = {0};
-    accum_time = {0};
-    fixed_time = {0, FIXED_TIME_LIMIT}; // 60Hz
+    struct dmo_time base_time = dmo_sys_time();
+    struct dmo_time total_time = {0};
+    struct dmo_time current_time = {0};
+    long accum_time = 0;
+    long frame_time = 0;
+    long fixed_time = FIXED_TIME_LIMIT;
 
     // struct dmo_state previous_state;
     // struct dmo_state current_state;
 
     while(running) {
-        dmo_time new_time = dmo_sys_time();
-        dmo_time frame_time = {0};
 
-        frame_time.seconds = new_time.seconds - current_time.seconds;
-        frame_time.nanoseconds = new_time.nanoseconds - current_time.nanoseconds;
-        if((frame_time.seconds > 0) || (frame_time.nanoseconds > FRAME_TIME_CLAMP)) {
-            frame_time.seconds = 0;
-            frame_time.nanoseconds = FRAME_TIME_CLAMP;
+        dmo_time new_time = dmo_sys_time();
+
+        frame_time = new_time.nanoseconds - current_time.nanoseconds;
+        if(frame_time > FRAME_TIME_CLAMP) {
+            frame_time = FRAME_TIME_CLAMP;
         }
         
         total_time.seconds = new_time.seconds - base_time.seconds;
         total_time.nanoseconds = new_time.nanoseconds - base_time.nanoseconds;
+        (void)total_time;
 
         current_time.seconds = new_time.seconds;
         current_time.nanoseconds = new_time.nanoseconds;
 
-        accum_time.seconds += frame_time.seconds;
-        accum_time.nanoseconds += frame_time.nanoseconds;
+        accum_time += frame_time;
 
-        while((accum_time.seconds >= fixed_time.seconds) && (accum_time.nanoseconds >= fixed_time.nanoseconds)) {
+        while(accum_time >= fixed_time) {
 
             //previous_state = current_state;
 
             // simulate current_state with fixed_time;
             // dmo_sim_step(current_state, total_time, fixed_time);
             
-            accum_time.seconds -= fixed_time.seconds;
-            accum_time.nanoseconds -= fixed_time.nanoseconds;
+            accum_time -= fixed_time;
         }
 
-        const double alpha_seconds = accum_time.seconds / fixed_time.seconds;
-        const double alpha_nanoseconds = accum_time.nanoseconds / fixed_time.nanoseconds;
-        (void)alpha_seconds; 
-        (void)alpha_nanoseconds;
+        const double alpha_time = accum_time / fixed_time;
+        (void)alpha_time;
         
         // dmo_state alpha_state = current_state * alpha + previous_state * (1.0 - alpha_state);
         // dmo_gfx_render(current_state);
